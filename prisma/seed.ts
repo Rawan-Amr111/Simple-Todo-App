95f0bdea4a89f66b13d4e3a10b1783afba50f253
Rawan Amr <rawanamr357@gmail.com>
2025-08-19 15:18:17 +0300

    fix seed error

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,12 +3,12 @@ import { fa, faker } from "@faker-js/faker";
 const prisma = new PrismaClient();
 
 async function main() {
-  await prisma.todo.createMany({
+  /* await prisma.todo.createMany({
     data: Array.from({ length: 25 }, () => ({
       title: faker.lorem.words({ min: 2, max: 5 }),
       body: faker.lorem.words({ min: 1, max: 10 }),
     })),
-  });
+  }); */
 }
 /* await prisma.user.createMany({
   data: Array.from({ length: 25 }, () => ({
